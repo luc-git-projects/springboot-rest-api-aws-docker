c0f2bb749c74a8c7f225b88bacfcc4b8e619a4e6
luc-git-projects <luc.git.projects@hotmail.com>
2023-10-28 09:22:26 -0300

    direrenças

--- 07-rest-with-spring-boot/src/main/java/br/com/lucotavio/restspringboot/model/Permission.java
+++ 07-rest-with-spring-boot/src/main/java/br/com/lucotavio/restspringboot/model/Permission.java
@@ -3,4 +3,5 @@ package br.com.lucotavio.restspringboot.model;
 public class Permission {
 
     private Long id;
+    private String description;
 }
--- 07-rest-with-spring-boot/src/main/java/br/com/lucotavio/restspringboot/model/User.java
+++ 07-rest-with-spring-boot/src/main/java/br/com/lucotavio/restspringboot/model/User.java
@@ -1,9 +1,49 @@
 package br.com.lucotavio.restspringboot.model;
 
-public class User {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class User implements UserDetails {
 
     private Long id;
     private String fullname;
 
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
